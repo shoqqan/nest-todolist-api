35f0552de406b24161c006c67d541993db1042e3
shoqqan <tataev.shokan@gmail.com>
2025-01-06 14:47:20 +0500

    [fix]: docker-compose

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todolists')
+    .setDescription('The MLG Todolists API description')
+    .setVersion('1.0')
+    .addTag('todolists')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(8080);
 }
 bootstrap();
